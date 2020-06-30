e2c8e01d7bd4f51f9a23d9cedfea6391c309367d
kajeagenstpi <jptagnepis@up.edu.ph>
2020-06-30 23:02:32 +0900

    added files

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update
+apt-get install squid3 apache2-utils
+cp /etc/squid/squid.conf /etc/squid/squid.conf.default
+touch /etc/squid/squid_passwd
+chown proxy /etc/squid/squid_passwd
