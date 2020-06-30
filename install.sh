c345648efb040d885da210569fb92fe299c0b700
Jake Tagnepis <jptagnepis@up.edu.ph>
2020-06-30 23:20:25 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install squid3 apache2-utils
+apt-get install squid3 apache2-utils -y
 cp /etc/squid/squid.conf /etc/squid/squid.conf.default
 touch /etc/squid/squid_passwd
 chown proxy /etc/squid/squid_passwd
